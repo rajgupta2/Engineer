708cd53d277809d4ce3ce47322aef1440fbf2388
Raj_Gupta <rajgupta.ckt22@gmail.com>
2024-02-18 17:26:21 +0530

    Adding some directory

---
+++ models/Notes.js
@@ -0,0 +1,120 @@
+var Notes=[
+    {
+        Subject: "Linux Commands",
+        Description: "Let's learn linux commands",
+        Notes: "/Notes/Command.txt"
+    },
+    {
+        Subject: "C",
+        Description: "Mother language.",
+        Notes: "/Notes/C.pdf",
+        Tutorial: "https://www.w3schools.com/c/index.php",
+        Practice: "https://www.w3schools.com/c/index.php"
+    },
+    {
+        Subject: "HTML",
+        Description: "Let’s Build.",
+        Notes: "/Notes/HTML.pdf",
+        Tutorial: "https://www.w3schools.com/html/default.asp",
+        Practice: "https://www.w3schools.com/html/default.asp"
+    },
+    {
+
+        Subject: "CSS",
+        Description: "Let’s Design.",
+        Notes: "/Notes/CSS.pdf",
+        Tutorial: "https://www.w3schools.com/css/default.asp",
+        Practice: "https://www.w3schools.com/css/default.asp"
+    },
+    {
+        Subject: "JavaScript",
+        Description: "Object based programming language",
+        Notes: "/Notes/JS.pdf",
+        Tutorial: "https://www.w3schools.com/js/default.asp",
+        Practice: "https://www.w3schools.com/js/default.asp"
+    },
+    {
+        Subject: "Bootstrap",
+        Description: "Let’s design responsive UI",
+        Notes: "/Notes/BootStrap.pdf",
+        Tutorial: "https://www.w3schools.com/bootstrap5/index.php",
+        Practice: "https://www.w3schools.com/bootstrap5/index.php"
+    },
+    {
+        Subject: "jQuery",
+        Description: "write less, do more.",
+        Notes: "/Notes/jQuery.pdf",
+        Tutorial: "https://www.w3schools.com/jquery/default.asp",
+        Practice: "https://www.w3schools.com/jquery/default.asp"
+    },
+    {
+        Subject: "SQL",
+        Description: "Database language.",
+        Notes: "/Notes/SQL.pdf",
+        Tutorial: "/Notes/DB-SQL.pdf",
+        Practice: "https://www.w3schools.com/sql/"
+    },
+    {
+        Subject: "XAMPP",
+        Description: "A set of tools",
+        Notes: "/Notes/XAMPP.txt",
+        Tutorial: "https://www.javatpoint.com/xampp"
+    },
+    {
+        Subject: "C# with OOPS",
+        Description: "A Programming Language",
+        Notes: "/Notes/C-sharp.pdf",
+        Tutorial: "https://www.tutorialsteacher.com/csharp"
+    },
+    {
+        Subject: ".Net MVC",
+        Description: "Let's create an application using C#",
+        Notes: "/Notes/dotNet MVC.pdf",
+        Tutorial: "https://www.tutorialsteacher.com/mvc"
+    },
+    {
+        Subject: "Node js",
+        Description: "A run time environment for JavaScript",
+        Notes: "/Notes/Nodejs.pdf",
+        Tutorial: "/Notes/nodejs.txt"
+    },
+    {
+        Subject: "WinSCP",
+        Description: "To transfer data from windows machine to Linux machine",
+        Notes: "/Notes/winscp.txt",
+    },
+     {
+        Subject: "Git", 
+        Description: "Expanding Philanthropy and the Social Economy",
+        Notes: "/Notes/git.txt", 
+        Tutorial: "https://www.w3schools.com/git/default.asp?remote:github",
+        Practice: "https://www.w3schools.com/git/default.asp?remote:github"
+    },
+    {
+        Subject: "Express", 
+        Description: "A node js framework ",
+        Notes: "/Notes/Express.txt"
+    },
+    {
+        Subject: "EJS", 
+        Description: "Embedded JavaScript for node js",
+        Notes: "/Notes/Express.txt"
+    },
+    {
+        Subject: "Mongo DB", 
+        Description: "Document oriented Database",
+        Notes: "/Notes/MongoDB.txt"
+    },
+    {
+        Subject: "Mongoose", 
+        Description: "A MongoDB Framework",
+        Notes: "/Notes/mongoose.txt"
+    },
+    {
+        Subject: "Tailwind CSS", 
+        Description: "Let's design UI with flexibility",
+        Tutorial: "https://tailwindcss.com/"
+    }
+]
+export default Notes;
+
