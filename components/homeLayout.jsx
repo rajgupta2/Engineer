d3b2f5b572386bdacdec0debc47d8cef9dc9ed8c
Raj Gupta <116866281+rajgupta2@users.noreply.github.com>
2025-03-19 11:29:23 +0530

    Making SEO friendly.

--- components/homeLayout.jsx
+++ components/homeLayout.jsx
@@ -1,19 +1,20 @@
 import React from  'react';
 import Navbar from './navbar'
 import Footer from './footer'   
+import Head from 'next/head'
 
 const HomeLayout = function ({ children }){
     return (
         <>
             <Navbar />
-             <head>
+             <Head>
                 <meta name="google-site-verification" content="-iz1XDJXPge2QUU9uoGTTTVJdCB0zytj6W6d5v2J-Gw" />
                 <title>Engineer | Organize & Share Study Materials</title>
                 <meta name="description" content="Engineer is a project designed for organizing and sharing study materials, notes, and tutorials. Built with Next.js for a clean, responsive experience." />
                 <meta name="keywords" content="study materials, engineering notes, tutorials, Next.js, GitHub Pages" />
                 <meta name="author" content="Raj Gupta" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            </head>
+            </Head>
             <main>{children}</main>
             <Footer />
         </>
