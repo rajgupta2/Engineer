a412cafd91393fea801efe98306cd4b1663f447d
Raj_Gupta <rajgupta.ckt22@gmail.com>
2025-03-20 16:56:52 +0530

    Adding More meta keywords to make seo friendly.

--- components/homeLayout.jsx
+++ components/homeLayout.jsx
@@ -1,6 +1,6 @@
 import React from  'react';
 import Navbar from './navbar'
-import Footer from './footer'   
+import Footer from './footer'
 import Head from 'next/head'
 
 const HomeLayout = function ({ children }){
@@ -11,7 +11,7 @@ const HomeLayout = function ({ children }){
                 <meta name="google-site-verification" content="-iz1XDJXPge2QUU9uoGTTTVJdCB0zytj6W6d5v2J-Gw" />
                 <title>Engineer | Organize & Share Study Materials</title>
                 <meta name="description" content="Engineer is a project designed for organizing and sharing study materials, notes, and tutorials. Built with Next.js for a clean, responsive experience." />
-                <meta name="keywords" content="study materials, engineering notes, tutorials, Next.js, GitHub Pages" />
+                <meta name="keywords" content="engineer, study materials, engineering notes, tutorials, Next.js, GitHub Pages, notes, sharing notes, tech notes" />
                 <meta name="author" content="Raj Gupta" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
